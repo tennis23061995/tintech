a23d01cc04277c4c843a3d0594651d25118b2c77
Phan Thanh Tam <rogerphan0623@gmail.com>
2017-11-27 23:05:12 +0700

    a

--- www/static/theme/liblog/res/src/js/business/layout/loadasyncimage.js
+++ www/static/theme/liblog/res/src/js/business/layout/loadasyncimage.js
@@ -1,8 +1,18 @@
-
-function init() {
-var imgDefer = document.getElementsByTagName('img');
-for (var i=0; i<imgDefer.length; i++) {
-if(imgDefer[i].getAttribute('data-src')) {
-imgDefer[i].setAttribute('src',imgDefer[i].getAttribute('data-src'));
-} } }
-window.onload = init;
+function loadScripts(urls, length, success){
+        if(length > 0){
+            script = document.createElement("SCRIPT");
+            script.src = urls[length-1];
+            console.log();
+            script.onload = function() {
+                console.log('%c Script: ' + urls[length-1] + ' loaded!', 'color: #4CAF50');
+                loadScripts(urls, length-1, success);
+            };
+            document.getElementsByTagName("body")[0].appendChild(script);
+        }
+        else
+            if(success)
+                success();
+    }
+    loadScripts(urls, urls.length, function(){
+      $('.timeago').timeago();
+  });
---
+++ www/static/theme/liblog/res/src/js/business/page/index.js
@@ -0,0 +1,6 @@
+$("#showContent img").removeAttr("style");
+$("#showContent img").removeAttr("width");
+$("#showContent img").removeAttr("height");
+$("#showContent img").css("width","100%");
+$("#showContent video").removeAttr("style");
+$("#showContent video").css("width","100%");
